fd8e0d1a80bed73971261627914b3d21a6761be4
Rafael Schlatter <rafael.schlatter@cognizant.com>
2019-05-08 11:08:55 +0200

    Test cont. deployment number 2.

--- application.py
+++ application.py
@@ -3,4 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Python flask website hosted on Azure Linux app service for free! Test continous deployment."
+    message = "Python flask website hosted on Azure Linux app service for free! \
+        Test continous deployment (works only with public git repo)"
+    return message
