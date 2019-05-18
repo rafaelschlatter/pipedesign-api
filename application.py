7b1a6ae7265be9bb4d21cb60bb041f72fcf08268
Rafael <rafaelschlatter@gmail.com>
2019-05-18 17:52:42 +0200

    Created virtualenv, added readme.

--- application.py
+++ application.py
@@ -1,8 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     message = "Python flask website hosted on Azure Linux app service for free! \
         Test continous deployment (works only with public git repo)"
     return message
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
