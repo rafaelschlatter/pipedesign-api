63857401aad174eafb41d6dbdcb427d27ee074ce
Rafael <rafaelschlatter@gmail.com>
2019-05-19 13:40:32 +0200

    Turned app into an API.

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
@@ -7,6 +7,13 @@ def hello():
     message = "Python flask website hosted on Azure Linux app service for free! \
         Test continous deployment (works only with public git repo)"
     return message
+
+
+@app.route("/pipedesignml/api/predict", methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = data["number"] * 2
+    return jsonify({"prediction": prediction})
 
 
 if __name__ == '__main__':
