077057e527a010def61e17a7029201eb2fb4d566
Rafael <rafaelschlatter@gmail.com>
2019-06-16 20:16:27 +0200

    More api tests.

---
+++ tests/apis/test_model.py
@@ -0,0 +1,21 @@
+import pytest
+import json
+from application import application
+
+
+class TestModel():
+    def test_model_current(self, client):
+        resp = client.get("/model/current/")
+        assert resp.status_code == 200
+        assert resp.json["Error"] == "Model has not been trained yet. Train model first."
+
+
+    def test_model_train_current_success(self, client):
+        resp = client.put("/model/train_current/50")
+        assert resp.status_code == 200
+        assert resp.json["training_result"] == "Success"
+
+
+    def test_model_train_current_with_invalid_parameter(self, client):
+        resp = client.put("/model/train_current/f")
+        assert resp.status_code == 500
