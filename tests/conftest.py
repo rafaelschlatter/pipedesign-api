6ed7a4e5d971d19dbf373484da2c0a4163d53abf
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-06-17 15:55:32 +0200

    More api tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+# Because flask app needs to be called "app" in order to work on Azure devops,
+# and pytest fixture needs to be named "app" in order to work with pytest-flask.
 from application import app as application
 
 @pytest.fixture
