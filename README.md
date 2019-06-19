69ce06c34e905d58be7cbb1d92f6a03d4800d80f
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-06-19 11:04:49 +0200

    Reduce code duplication.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![codecov](https://codecov.io/gh/rafaelschlatter/pipedesign-ml/branch/master/graph/badge.svg)](https://codecov.io/gh/rafaelschlatter/pipedesign-ml)
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/6f28fe09f64e4eaaa866498be841fa84)](https://app.codacy.com/app/rafaelschlatter/pipedesign-api?utm_source=github.com&utm_medium=referral&utm_content=rafaelschlatter/pipedesign-api&utm_campaign=Badge_Grade_Settings)
 
-This is a python flask API that is hosted on Microsoft Azure app service. The API allows to train a machine learning model and request predictions on new data. Please visit the base url at http://pipedesign.azurewebsites.net/ to see the Swagger api documentation.
+This is a python flask API that is hosted on Microsoft Azure app service. The API allows to train a machine learning model and request predictions on new data. Please visit the base url at <http://pipedesign.azurewebsites.net> to see the Swagger api documentation.
 
 ## Setup
 Azure app service on linux needs a **requirements.txt** file for all dependencies except flask. A pipenv workflow probably fails (not tested).
--- tests/apis/test_prediction.py
+++ tests/apis/test_prediction.py
@@ -1,4 +1,5 @@
 import json
+from tests.helper_functions import HelperFunctions
 
 
 class TestPrediction():
@@ -9,7 +10,7 @@ class TestPrediction():
 
 
     def test_prediction_predict_current_with_json(self, client):
-        pipedesign_json = self._helper_load_json()
+        pipedesign_json = HelperFunctions._load_json()
         resp = client.post("/prediction/predict_current/", json=pipedesign_json)
         assert resp.status_code == 200
         assert resp.json["prediction"] == "Viable"
@@ -25,16 +26,10 @@ class TestPrediction():
 
 
     def test_prediction_predict_pickled_with_json(self, client):
-        pipedesign_json = self._helper_load_json()
+        pipedesign_json = HelperFunctions._load_json()
         resp = client.post("prediction/predict_pickled/", json=pipedesign_json)
         assert resp.status_code == 200
         assert resp.json["prediction"] == "Viable"
         assert resp.json["confidence"] == "0.00985992289985627"
         assert resp.json["label"] == "1"
         assert resp.json["pipedesign_id"] == "0a234fea9682454facab730c0a7f83f0"
-
-
-    def _helper_load_json(self):
-        with open("data/json/0a234fea9682454facab730c0a7f83f0.json") as json_file:
-            pipedesign_json = json.load(json_file)
-        return pipedesign_json
