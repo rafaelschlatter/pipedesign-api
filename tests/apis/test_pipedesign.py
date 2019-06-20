fa715bd6401a1febf54054a4f6a11e58b1aae073
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-06-20 10:14:27 +0200

    Unified behaviour of blobhandler methods.

--- tests/apis/test_pipedesign.py
+++ tests/apis/test_pipedesign.py
@@ -7,6 +7,5 @@ class TestPipedesign():
 
     def test_get_pipedesign_with_invalid_id(self, client):
         resp = client.get("/pipedesign/fake_id123")
-        assert resp.status_code == 200
-        assert resp.json["Error"] == "The pipedesign with the given id does not exist in Azure blob."
+        assert resp.status_code == 404
     
