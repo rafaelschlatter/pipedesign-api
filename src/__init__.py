436fe13d44b4abc349cbcd17433afba3a9b20504
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-07-01 12:09:49 +0200

    Proper place for creating api and app.

--- src/__init__.py
+++ src/__init__.py
@@ -1,21 +1,5 @@
 from flask import Flask
-from flask_restplus import Api
-
-from .apis.prediction import api as ns1
-from .apis.ml_model import api as ns2
-from .apis.pipedesign import api as ns3
-
-
-api = Api(
-    version='0.1',
-    title='Pipedesign ML Api',
-    description='An API to retrieve predictions about the constructability of pipe systems. Maintainer: rafaelschlatter@gmail.com.',
-    contact="rafaelschlatter@gmail.com"
-)
-
-api.add_namespace(ns1)
-api.add_namespace(ns2)
-api.add_namespace(ns3)
+from src.apis import api
 
 
 def create_app():
---
+++ src/apis/__init__.py
@@ -0,0 +1,17 @@
+from flask_restplus import Api
+
+from .prediction import api as ns1
+from .ml_model import api as ns2
+from .pipedesign import api as ns3
+
+
+api = Api(
+    version='0.1',
+    title='Pipedesign ML Api',
+    description='An API to retrieve predictions about the constructability of pipe systems. Maintainer: rafaelschlatter@gmail.com.',
+    contact="rafaelschlatter@gmail.com"
+)
+
+api.add_namespace(ns1)
+api.add_namespace(ns2)
+api.add_namespace(ns3)
