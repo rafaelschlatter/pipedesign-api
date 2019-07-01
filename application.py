6a775fe05143937cc0110b609876e27ccad493a6
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-07-01 13:22:02 +0200

    Finally...

--- application.py
+++ application.py
@@ -1,4 +1,10 @@
 from flask import Flask
 from src import create_app
 
-app = create_app("prod")
+# For Azure app service
+app = create_app(config_name="prod")
+
+# For easy local testing
+if __name__ == '__main__':
+    local_app = create_app(config_name="dev")
+    local_app.run(debug=True)
