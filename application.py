7cfa44d800de6aefd5a0c6564808b0b7f756bfce
Rafael Schlatter <rafaelschlatter@gmail.com>
2019-07-01 12:55:13 +0200

    Finally getting the configuration to work on azure.

--- application.py
+++ application.py
@@ -1,15 +1,5 @@
 from flask import Flask
-from src.apis import api
-from src.config import config_by_name
+from src import create_app
 
-
-def create_app(config_name="dev"):
-    app = Flask(__name__)
-    app.config.from_object(config_by_name[config_name])
-    api.init_app(app)
-
-    return app
-
-if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+app = create_app("prod")
+app.run()
