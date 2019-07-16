4bcac6290df7de557790586b3a953372d653f66a
Rafael <rafaelschlatter@gmail.com>
2019-07-16 19:51:18 +0200

    Codestyle black on tests.

--- tests/ml/test_model.py
+++ tests/ml/test_model.py
@@ -5,10 +5,12 @@ from src.ml import preprocessor, model
 from src.infrastructure import blobhandler
 
 
-class TestModel():
+class TestModel:
     def test_train(self):
         handler = blobhandler.BlobHandler()
-        pipedesigns = handler.download_blobs(os.environ["CONTAINER_NAME_DATA"], number_of_blobs=50)
+        pipedesigns = handler.download_blobs(
+            os.environ["CONTAINER_NAME_DATA"], number_of_blobs=50
+        )
         proc = preprocessor.Preprocessor()
         dataset = proc.create_training_data(pipedesigns)
         clf = model.Model()
@@ -17,17 +19,21 @@ class TestModel():
         assert isinstance(clf.classifier, RandomForestClassifier)
         assert isinstance(clf.last_train_time_utc, datetime)
 
-
     def test_predict(self):
         handler = blobhandler.BlobHandler()
-        pipedesigns = handler.download_blobs(os.environ["CONTAINER_NAME_DATA"], number_of_blobs=50)
+        pipedesigns = handler.download_blobs(
+            os.environ["CONTAINER_NAME_DATA"], number_of_blobs=50
+        )
         proc = preprocessor.Preprocessor()
         dataset = proc.create_training_data(pipedesigns)
         clf = model.Model()
         clf.train(training_data=dataset)
 
-        test_data = handler.azure_blob_to_json(container_name=os.environ["CONTAINER_NAME_DATA"], blob_name="test_blob_do_not_delete")[1]
+        test_data = handler.azure_blob_to_json(
+            container_name=os.environ["CONTAINER_NAME_DATA"],
+            blob_name="test_blob_do_not_delete",
+        )[1]
         label, confidence = clf.predict(json_data=test_data)
         # TODO: This depends on the currently trained model, think of a fix here.
-        assert (label[0] == 0 or label[0] == 1)
-        assert (confidence[0][0] <= 1 and confidence[0][0] >= 0)
+        assert label[0] == 0 or label[0] == 1
+        assert confidence[0][0] <= 1 and confidence[0][0] >= 0
--- tests/ml/test_preprocessor.py
+++ tests/ml/test_preprocessor.py
@@ -4,7 +4,7 @@ from src.infrastructure import blobhandler
 from src.ml import preprocessor
 
 
-class TestPreprocessor():
+class TestPreprocessor:
     def test_create_training_data(self):
         handler = blobhandler.BlobHandler()
         pipedesigns = handler.download_blobs(os.environ["CONTAINER_NAME_DATA"])
@@ -12,10 +12,12 @@ class TestPreprocessor():
         dataset = proc.create_training_data(pipedesigns)
         assert isinstance(dataset, pd.DataFrame)
 
-
     def test_flatten_pipesegments(self):
         handler = blobhandler.BlobHandler()
-        json = handler.azure_blob_to_json(container_name=os.environ["CONTAINER_NAME_DATA"], blob_name="test_blob_do_not_delete")[1]
+        json = handler.azure_blob_to_json(
+            container_name=os.environ["CONTAINER_NAME_DATA"],
+            blob_name="test_blob_do_not_delete",
+        )[1]
         proc = preprocessor.Preprocessor()
         pipedesign_sample = proc.flatten_pipesegments(json)
         assert isinstance(pipedesign_sample, pd.DataFrame)
